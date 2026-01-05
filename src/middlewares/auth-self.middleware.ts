038a0f3aaf1a31689c9655a78c182bd4b83c735d
somxai <sai52237754@gmail.com>
2026-01-05 11:14:03 +0700

    refactor v1.0.2

--- src/middlewares/auth-self.middleware.ts
+++ src/middlewares/auth-self.middleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 
 const authenticateSelf = (req: any, res: Response, next: NextFunction) => {
     if (!req.user || !req.user.userId) {
